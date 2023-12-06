08cd75807ebb41775238c7311e17892dfb48c780
23tae <taehoonkim.dev@gmail.com>
2023-12-06 23:24:29 +0900

    Refactor: 함수 모듈화

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 from dotenv import load_dotenv
-import os 
+import os
 from instagrapi import Client
 
-# Load env file
-load_dotenv()
-ACCOUNT_USERNAME = os.getenv('ACCOUNT_USERNAME')
-ACCOUNT_PASSWORD = os.getenv('ACCOUNT_PASSWORD')
 
-# Login to Instagram
-cl = Client()
-cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
+def main():
+    # Load env file
+    load_dotenv()
+    ACCOUNT_USERNAME = os.getenv('ACCOUNT_USERNAME')
+    ACCOUNT_PASSWORD = os.getenv('ACCOUNT_PASSWORD')
+    print('name: ', ACCOUNT_USERNAME)
 
-# Get posts
-user_id = cl.user_id_from_username(ACCOUNT_USERNAME)
-medias = cl.user_medias(user_id, 5)
+    # Login to Instagram
+    cl = Client()
+    try:
+        cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
+    except Exception as e:
+        print('Error 1: ', e)
+        return
+
+    # Get posts
+    try:
+        user_id = cl.user_id_from_username(ACCOUNT_USERNAME)
+        print(user_id)
+    except Exception as e:
+        print('Error 2: ', e)
+        return
+    # medias = cl.user_medias(user_id, 5)
+
+
+if __name__ == "__main__":
+    main()
