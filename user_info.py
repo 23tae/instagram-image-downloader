2fa1b0b8be536a28743a7dadb8b5f6901224d80d
23tae <taehoonkim.dev@gmail.com>
2023-12-12 18:40:51 +0900

    Chore: env 변수명 변경

--- user_info.py
+++ user_info.py
@@ -4,6 +4,6 @@ import os
 
 def get_account() -> dict:
     load_dotenv()
-    username = os.getenv('ACCOUNT_USERNAME')
-    password = os.getenv('ACCOUNT_PASSWORD')
+    username = os.getenv('INSTAGRAM_USERNAME')
+    password = os.getenv('INSTAGRAM_PASSWORD')
     return ({'username': username, 'password': password})
